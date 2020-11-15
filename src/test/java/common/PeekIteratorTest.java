8ff9271ae4194492f7f7a2f6ce098ac5591a3545
idcpj <260083304@qq.com>
2020-11-15 21:44:50 +0800

    add test

--- src/test/java/common/PeekIteratorTest.java
+++ src/test/java/common/PeekIteratorTest.java
@@ -1,7 +1,6 @@
 package common;
 
 
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,21 +8,47 @@ import static org.junit.Assert.assertEquals;
 public class PeekIteratorTest {
 
     @Test
-    public void test_peak(){
+    public void test_peak() {
         String source = "abcdefg";
         PeekIterator<Character> it = new PeekIterator<>(source.chars().mapToObj(c -> (char) c), (char) 0);
-        Character next = it.next();
 
-        assertEquals('a', next.charValue());
+        assertEquals('a', it.next().charValue());
+        assertEquals('b', it.next().charValue());
+
+        it.next();
+        it.next();
+
+        assertEquals('e', it.next().charValue());
+        assertEquals('f', it.peek().charValue());
+        assertEquals('f', it.peek().charValue());
+        assertEquals('f', it.next().charValue());
+        assertEquals('g', it.next().charValue());
+    }
+    @Test
+    public  void test_lookahead2(){
+        String source = "abcdefg";
+        PeekIterator<Character> it = new PeekIterator<>(source.chars().mapToObj(c -> (char) c),(char)0);
+
+        assertEquals('a',it.next().charValue());
         assertEquals('b',it.next().charValue());
+        assertEquals('c',it.next().charValue());
+        it.putBack();
+        it.putBack();
+        assertEquals('b',it.next().charValue());
+    }
 
-        it.next();
-        it.next();
-
-        assertEquals('e',it.next().charValue());
-        assertEquals('f',it.peek().charValue());
-        assertEquals('f',it.peek().charValue());
-        assertEquals('f',it.next().charValue());
-        assertEquals('g',it.next().charValue());
+    @Test
+    public void test_endToken(){
+        String source = "abcdefg";
+        PeekIterator<Character> it = new PeekIterator<>(source.chars().mapToObj(c -> (char) c), (char) 0);
+        int i =0;
+        while(it.hasNext()){
+            if (i==source.length()){
+                assertEquals((char)0,it.next().charValue());
+            }else{
+                assertEquals(source.charAt(i),it.next().charValue());
+                i++;
+            }
+        }
     }
 }
